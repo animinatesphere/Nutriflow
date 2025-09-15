c65420a939f38e1b175f0446adff7b35df6976e8
Adeyemi Pelumi <adeyemipelumi1980@gmail.com>
2025-09-15 14:59:59 +0100

    Add notification handling to LoginForm and implement loading spinner

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tagger from "@dhiwise/component-tagger";
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
+
   build: {
     outDir: "build",
     chunkSizeWarningLimit: 3000,
@@ -16,16 +16,13 @@ export default defineConfig({
   plugins: [tsconfigPaths(), react(), tagger()],
   css: {
     postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer,
-      ],
+      plugins: [tailwindcss, autoprefixer],
     },
   },
   server: {
     port: "4028",
     host: "0.0.0.0",
     strictPort: true,
-    allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
-  }
+    allowedHosts: [".amazonaws.com", ".builtwithrocket.new"],
+  },
 });
